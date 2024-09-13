28e63acd4789d997bb5ccb9b6608fe2adf1b3048
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-09-13 16:01:38 +0200

    add font

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -8,10 +8,10 @@ import { CommonModule } from '@angular/common';
     imports: [CommonModule],
     template: `
     <section>
-        <h1>SAKURA RAMEN</h1>
-        <h2>BEST RAMEN IN TOWN</h2>
+        <h1 class="fontRaleway">SAKURA RAMEN</h1>
+        <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>`,
-    styleUrls: ["./landingPage.component.scss"]
+    styleUrls: ['./landingPage.component.scss']
 })
 
 export class LandingPageComponent {}
