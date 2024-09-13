c7e1fc77853157dd9c47993888020057d4ae4713
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-09-13 14:43:30 +0200

    test Lauf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from './landingPage/landingPage.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
